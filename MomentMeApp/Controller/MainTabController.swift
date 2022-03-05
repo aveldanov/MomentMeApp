e275c5d376976186ce543f89bf148364d2655d4c
Anton Veldanov <anton.veldanov@gmail.com>
2022-03-04 20:07:24 -0800

    iOS

---
+++ MomentMeApp/Controller/MainTabController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabController.swift
+//  MomentMeApp
+//
+//  Created by Anton Veldanov on 3/4/22.
+//
+
+import UIKit
+
+
+class MainTabController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
+
+
