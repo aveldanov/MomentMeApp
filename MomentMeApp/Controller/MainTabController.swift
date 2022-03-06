a10f88ff26be7ab9b5032b21f0d1690fec15fa7b
Anton Veldanov <anton.veldanov@gmail.com>
2022-03-06 13:05:57 -0800

    iOS

--- MomentMeApp/Controller/MainTabController.swift
+++ MomentMeApp/Controller/MainTabController.swift
@@ -22,17 +22,17 @@ class MainTabController: UITabBarController{
     
     func configureViewControllers(){
         view.backgroundColor = .white
-        let feed = FeedController()
-        let search = SearchController()
-        let imageSelctor = ImageSelectorController()
-        let notifications = NotificationsController()
-        let profile = ProfilerController()
+        let feed = templateNavigaitonController(unselectedImage: #imageLiteral(resourceName: "home_unselected") , selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController())
+        let search = templateNavigaitonController(unselectedImage: #imageLiteral(resourceName: "search_unselected") , selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
+        let imageSelctor = templateNavigaitonController(unselectedImage: #imageLiteral(resourceName: "plus_unselected") , selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
+        let notifications = templateNavigaitonController(unselectedImage: #imageLiteral(resourceName: "like_unselected") , selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationsController())
+        let profile = templateNavigaitonController(unselectedImage: #imageLiteral(resourceName: "profile_unselected") , selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfilerController())
         
         viewControllers = [feed, search, imageSelctor, notifications, profile]
     }
     
     
-    func templeteNavigaitonController(unselectedImage:UIImage, selectedImage: UIImage, rootViewController: UIViewController)->UINavigationController{
+    func templateNavigaitonController(unselectedImage:UIImage, selectedImage: UIImage, rootViewController: UIViewController)->UINavigationController{
         
         let nav = UINavigationController(rootViewController: rootViewController)
         nav.tabBarItem.image = unselectedImage
