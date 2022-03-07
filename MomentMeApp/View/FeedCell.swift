2832368ff3747b894f9cf876f39d0fc017494fa6
Anton Veldanov <anton.veldanov@gmail.com>
2022-03-06 21:55:44 -0800

    iOS

--- MomentMeApp/View/FeedCell.swift
+++ MomentMeApp/View/FeedCell.swift
@@ -13,12 +13,26 @@ class FeedCell: UICollectionViewCell{
     // MARK: - Properties
     static let identifier = "FeedCell"
     
+    private let profileImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.backgroundColor = .systemPurple
+        return imageView
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemPurple
+        backgroundColor = .white
+        addSubview(profileImageView)
+        profileImageView.anchor(top:topAnchor,
+                                left: leftAnchor,
+                                paddingTop: 12,
+                                paddingLeft: 12)
     }
     
     required init?(coder: NSCoder) {
