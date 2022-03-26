a04d64a3c1ee6297cd10dadd2f81c700496d8294
Anton Veldanov <anton.veldanov@gmail.com>
2022-03-25 20:46:12 -0700

    iOS

--- MomentMeApp/API/ImageUploader.swift
+++ MomentMeApp/API/ImageUploader.swift
@@ -11,8 +11,17 @@ import Firebase
 struct ImageUploader{
     static func uploadImage(image: UIImage, completeion: @escaping (String)->Void){
         
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else{
+            return
+        }
+        let filename = NSUUID().uuidString
         
-        
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
+        ref.putData(imageData, metadata: nil) { metadata, error in
+            if let error = error{
+                print("[ImageUploader] error",error.localizedDescription)
+            }
+        }
     }
     
     
