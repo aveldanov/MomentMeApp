36014b119b7b1a4769c72290db20ab8ef0498256
Anton Veldanov <anton.veldanov@gmail.com>
2022-04-02 10:54:14 -0700

    iOS

---
+++ MomentMeApp/API/UserService.swift
@@ -0,0 +1,16 @@
+//
+//  UserService.swift
+//  MomentMeApp
+//
+//  Created by Anton Veldanov on 4/2/22.
+//
+
+import Foundation
+
+
+struct UserService{
+    static func fetchUser(){
+        
+    }
+    
+}
---
+++ MomentMeApp/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  MomentMeApp
+//
+//  Created by Anton Veldanov on 4/2/22.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("users")
