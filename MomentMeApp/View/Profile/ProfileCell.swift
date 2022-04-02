62a56f968eae15307b7365bc157bbe9cac89b891
Anton Veldanov <anton.veldanov@gmail.com>
2022-04-01 20:55:07 -0700

    iOS

--- MomentMeApp/View/Profile/ProfileCell.swift
+++ MomentMeApp/View/Profile/ProfileCell.swift
@@ -12,11 +12,21 @@ class ProfileCell: UICollectionViewCell{
     // MARK: - Properties
     static let identifier = "ProfileCell"
     
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     // MARK: - :Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
--- MomentMeApp/View/Profile/ProfileHeader.swift
+++ MomentMeApp/View/Profile/ProfileHeader.swift
@@ -14,12 +14,39 @@ class ProfileHeader: UICollectionReusableView{
     // MARK: - Properties
     static let identifier = "ProfileHeader"
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Anton"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
     // MARK: - :Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .systemPink
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,paddingTop: 16, paddingLeft: 12)
+        profileImageView.setDimensions(height: 80, width: 80)
+        profileImageView.layer.cornerRadius = 80/2
+        
+        addSubview(nameLabel)
+        
+        nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        nameLabel.centerX(inView: <#T##UIView#>)
+        
+        
     }
     
     required init?(coder: NSCoder) {
