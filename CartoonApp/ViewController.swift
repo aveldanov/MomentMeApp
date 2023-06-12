69fdd43af89e22b228c4683dff4ef42e3ebdcfd0
Anton Veldanov <aveldanov@linkedin.com>
2023-06-11 17:56:50 -0700

    first commit

---
+++ CartoonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CartoonApp
+//
+//  Created by Anton Veldanov on 6/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
