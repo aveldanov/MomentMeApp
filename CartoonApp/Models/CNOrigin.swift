1071284ce8b01f2ba6b3369e29e705ba6cedd4f2
Anton Veldanov <anton.veldanov@gmail.com>
2023-06-18 19:54:33 -0700

    iOS

---
+++ CartoonApp/Models/CNOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  CNOrigin.swift
+//  CartoonApp
+//
+//  Created by Anton Veldanov on 6/18/23.
+//
+
+import Foundation
+
+struct CNOrigin: Codable {
+    let name: String
+    let url: String
+}
