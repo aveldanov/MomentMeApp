fc0abd47bb8d42a757c3f60cf16c2a572bd216cc
Anton Veldanov <anton.veldanov@gmail.com>
2023-07-03 18:19:07 -0700

    iOS

--- CartoonApp/Managers/CNImageLoader.swift
+++ CartoonApp/Managers/CNImageLoader.swift
@@ -10,18 +10,33 @@ import Foundation
 final class CNImageLoader {
     static let shared = CNImageLoader()
 
+    private var imageDataCache = NSCache<NSString, NSData>()
+
     private init() {
 
     }
 
+    /// Get Image content with URL
+    /// - Parameters:
+    ///   - url: source url
+    ///   - completion: callback
     func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key) {
+            print("Reading from cache: \(key)")
+            completion(.success(data as Data))
+            return
+        }
+
         let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = URLSession.shared.dataTask(with: request) {[weak self] data, response, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? URLError(.badServerResponse)))
                 return
             }
+            let value = data as NSData
 
+            self?.imageDataCache.setObject(value, forKey: key)
             completion(.success(data))
         }
 
