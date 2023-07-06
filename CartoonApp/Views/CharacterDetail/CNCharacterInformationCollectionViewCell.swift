c046c4c5cae6f29f0c4feddaebb42400141c1b3b
Anton Veldanov <anton.veldanov@gmail.com>
2023-07-05 21:30:51 -0700

    iOS

--- CartoonApp/Views/CharacterDetail/CNCharacterInformationCollectionViewCell.swift
+++ CartoonApp/Views/CharacterDetail/CNCharacterInformationCollectionViewCell.swift
@@ -11,10 +11,36 @@ class CNCharacterInformationCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "CNCharacterInformationCollectionViewCell"
 
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Earth"
+        return label
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Location"
+        return label
+    }()
+
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "globe.americas")
+        return imageView
+    }()
+
+    private let titleContainerView: UIView = {
+        let view = UIView()
+        return view
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .tertiarySystemBackground
+        titleContainerView.backgroundColor = .secondarySystemBackground
         layer.cornerRadius = 8
+        setupViewHierarchy()
+        setupViewLayout()
     }
 
     required init?(coder: NSCoder) {
@@ -23,14 +49,28 @@ class CNCharacterInformationCollectionViewCell: UICollectionViewCell {
 
     override func prepareForReuse() {
         super.prepareForReuse()
+//        valueLabel.text = nil
+//        titleLabel.text = nil
+//        iconImageView.image = nil
     }
 
     private func setupViewHierarchy() {
-
+        contentView.addSubviews(titleContainerView, valueLabel, iconImageView)
+        titleContainerView.addSubview(titleLabel)
     }
 
     private func setupViewLayout() {
+        valueLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleContainerView.translatesAutoresizingMaskIntoConstraints = false
 
+        NSLayoutConstraint.activate([
+            titleContainerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleContainerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33)
+        ])
     }
 
     public func configure(with: CNCharacterInformationCollectionViewViewModel) {
